10bcfa02f109d000d30a8be434be039027e36780
Unknown <riki.redz@gmail.com>
2017-10-29 21:38:13 +0100

    Added uploadGame function

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-exports.uploadGame = function() {
-    console.log("This is a message from the demo package");
-    return "This is a String";
+var request = require('request');
+
+exports.uploadGame = function(game) {
+    let username = "weathered-defender-8628";
+    let token = "YDWSoz3xdbsdV2ACSxwa";
+    const gameTest = {
+        "result": {
+            "mode": "ranked",
+            "hero": "Shaman",
+            "hero_deck": "Token",
+            "opponent": "Hunter",
+            "opponent_deck": "Midrange",
+            "coin": true,
+            "win": true,
+            "duration": 228,
+            "card_history": []
+        }
+    }
+
+    console.log(JSON.stringify(gameTest));
+    console.log(username);
+    console.log(token);
+
+    var options = {
+        url: 'https://trackobot.com/profile/results.json?username=' + username + '&token=' + token,
+        body: JSON.stringify(gameTest),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    request.post(options, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+
+        return body
+    })
+
+    //return { "success": true, "message": "Game uploaded with success" };
 }
--- test/index.js
+++ test/index.js
@@ -2,9 +2,48 @@ const test = require('tap').test;
 
 const easyTrackOBot = require('../index.js');
 
-test('smoke test', function(t) {
-    const string = easyTrackOBot.uploadGame();
-    t.is(typeof string, 'string');
-    t.ok(string.length > 0);
+test('upload_game test', function(t) {
+
+    const gameOfTestCorrect = {
+        "result": {
+            "hero": "Shaman",
+            "opponent": "Warrior",
+            "mode": "ranked",
+            "coin": true,
+            "win": true,
+            "duration": 634,
+            "card_history": [{
+                    "card_id": "EX1_405",
+                    "player": "opponent"
+                },
+                {
+                    "card_id": "GAME_005",
+                    "player": "me",
+                    "turn": 3
+                }
+            ]
+        }
+    }
+
+    const gameOfTestIncorrect = {
+        "result": ["This is obiviously not correct"]
+    }
+
+    const resultCorrect = easyTrackOBot.uploadGame(gameOfTestCorrect);
+    t.is(typeof resultCorrect, 'object');
+    t.ok(Object.keys(resultCorrect).length > 0);
+    t.ok(resultCorrect.hasOwnProperty("success"))
+    t.ok(resultCorrect.hasOwnProperty("message"))
+    t.equal(resultCorrect.success, true);
+    t.equal(resultCorrect.message, "Game uploaded with success");
+
+    const resultIncorrect = easyTrackOBot.uploadGame(gameOfTestIncorrect);
+    t.is(typeof resultIncorrect, 'object');
+    t.ok(Object.keys(resultIncorrect).length > 0);
+    t.ok(resultIncorrect.hasOwnProperty("success"))
+    t.ok(resultIncorrect.hasOwnProperty("message"))
+    t.equal(resultIncorrect.success, false);
+    t.equal(resultIncorrect.message, "Game inserted is invalid");
+
     t.end();
 })
