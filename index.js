bd96d3b3882351295b24c6ef127d069dd9486100
Riccardo <riki.redz@gmail.com>
2017-10-31 23:56:28 +0100

    Fixed for standard

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
 var request = require('request-promise-native')
 
-exports.uploadGame = function(game, responseReadyCallback) {
-    let username = "twilight-ysera-2686" //debug - TODO: REMOTE IT
-    let token = "Rg22pzUVixQpm1aXJZCV" //debug - TODO: REMOVE IT
-        /* ----- DEBUG ----- */
-    const gameTest = {
-        "result": {
-            "mode": "ranked",
-            "hero": "Shaman",
-            "hero_deck": "Token",
-            "opponent": "Hunter",
-            "opponent_deck": "Midrange",
-            "coin": true,
-            "win": true,
-            "duration": 228,
-            "card_history": []
-        }
-    }
+exports.uploadGame = function (game, responseReadyCallback) {
+  let username = 'twilight-ysera-2686' // debug - TODO: REMOTE IT
+  let token = 'Rg22pzUVixQpm1aXJZCV' // debug - TODO: REMOVE IT
 
     /* ------ DEBUG ------ */
-    //console.log(JSON.stringify(game));
-    //console.log(username);
-    //console.log(token);
+    // console.log(JSON.stringify(game));
+    // console.log(username);
+    // console.log(token);
 
-    var options = {
-        method: 'POST',
-        uri: 'https://trackobot.com/profile/results.json?username=' + username + '&token=' + token,
-        body: game,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        resolveWithFullResponse: true,
-        json: true
-    }
+  var options = {
+    method: 'POST',
+    uri: 'https://trackobot.com/profile/results.json?username=' + username + '&token=' + token,
+    body: game,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    resolveWithFullResponse: true,
+    json: true
+  }
 
-    request(options)
-        .then(function(response) {
-            let rsp = {
-                "success": true,
-                "code": response.statusCode,
-                "message": "Game uploaded with success",
-                "data": response.body
-            }
-            responseReadyCallback(rsp)
-        }).catch(function(response) {
-            let rsp = {
-                "success": false,
-                "code": response.error.status,
-                "message": response.error.error,
-                "data": null
-            }
-            responseReadyCallback(rsp)
+  request(options)
+        .then(function (response) {
+          let rsp = {
+            'success': true,
+            'code': response.statusCode,
+            'message': 'Game uploaded with success',
+            'data': response.body
+          }
+          responseReadyCallback(rsp)
+        }).catch(function (response) {
+          let rsp = {
+            'success': false,
+            'code': response.error.status,
+            'message': response.error.error,
+            'data': null
+          }
+          responseReadyCallback(rsp)
         })
 }
--- test/index.js
+++ test/index.js
@@ -2,65 +2,62 @@ const test = require('tap').test
 
 const easyTrackOBot = require('../index.js')
 
-test('upload_game_OK test', function(t) {
-
-    const gameOfTestCorrect = {
-        "result": {
-            "mode": "ranked",
-            "hero": "Shaman",
-            "hero_deck": "Token",
-            "opponent": "Hunter",
-            "opponent_deck": "Midrange",
-            "coin": true,
-            "win": true,
-            "duration": 228,
-            "card_history": []
-        }
+test('upload_game_OK test', function (t) {
+  const gameOfTestCorrect = {
+    'result': {
+      'mode': 'ranked',
+      'hero': 'Shaman',
+      'hero_deck': 'Token',
+      'opponent': 'Hunter',
+      'opponent_deck': 'Midrange',
+      'coin': true,
+      'win': true,
+      'duration': 228,
+      'card_history': []
     }
+  }
 
-    easyTrackOBot.uploadGame(
+  easyTrackOBot.uploadGame(
         gameOfTestCorrect,
         (result) => {
-            t.is(typeof result, 'object')
-            t.ok(Object.keys(result).length > 0)
+          t.is(typeof result, 'object')
+          t.ok(Object.keys(result).length > 0)
 
-            t.ok(result.hasOwnProperty("success"))
-            t.ok(result.hasOwnProperty("message"))
-            t.ok(result.hasOwnProperty("code"))
-            t.ok(result.hasOwnProperty("data"))
+          t.ok(result.hasOwnProperty('success'))
+          t.ok(result.hasOwnProperty('message'))
+          t.ok(result.hasOwnProperty('code'))
+          t.ok(result.hasOwnProperty('data'))
 
-            t.equal(result.success, true)
-            t.equal(result.message, "Game uploaded with success")
-            t.equal(result.code, 201)
-            t.ok(result.data.hasOwnProperty("result"))
+          t.equal(result.success, true)
+          t.equal(result.message, 'Game uploaded with success')
+          t.equal(result.code, 201)
+          t.ok(result.data.hasOwnProperty('result'))
 
-            t.end()
+          t.end()
         }
     )
-
 })
 
-test('upload_game_KO test', function(t) {
-    const gameOfTestIncorrect = {
-        "result": ["This is obiviously not correct"]
-    }
+test('upload_game_KO test', function (t) {
+  const gameOfTestIncorrect = {
+    'result': ['This is obiviously not correct']
+  }
 
-    easyTrackOBot.uploadGame(
+  easyTrackOBot.uploadGame(
         gameOfTestIncorrect,
         (result) => {
-            t.is(typeof result, 'object')
-            t.ok(Object.keys(result).length > 0)
+          t.is(typeof result, 'object')
+          t.ok(Object.keys(result).length > 0)
 
-            t.ok(result.hasOwnProperty("success"))
-            t.ok(result.hasOwnProperty("message"))
-            t.ok(result.hasOwnProperty("code"))
-            t.ok(result.hasOwnProperty("data"))
+          t.ok(result.hasOwnProperty('success'))
+          t.ok(result.hasOwnProperty('message'))
+          t.ok(result.hasOwnProperty('code'))
+          t.ok(result.hasOwnProperty('data'))
 
-            t.equal(result.success, false)
-            t.ok(result.code >= 300)
+          t.equal(result.success, false)
+          t.ok(result.code >= 300)
 
-            t.end()
+          t.end()
         }
     )
-
 })
