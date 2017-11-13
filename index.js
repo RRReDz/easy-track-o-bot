6ce7cb5aa6c82bf7938fedffe9b24612210d72b6
Riccardo <riki.redz@gmail.com>
2017-11-14 00:32:44 +0100

    Added getStats function, coverage, standard e tests to be completed

--- index.js
+++ index.js
@@ -2,72 +2,170 @@
 
 var request = require('request-promise-native')
 
-function EasyTrackOBot (username, token) {
-  this.username = username
-  this.token = token
+function EasyTrackOBot(username, token) {
+    this.username = username
+    this.token = token
 }
 
-EasyTrackOBot.prototype.uploadGame = function (game, responseReadyCallback) {
+EasyTrackOBot.prototype.uploadGame = function(game, responseReadyCallback) {
     /* ------ DEBUG ------ */
     // console.log(JSON.stringify(game));
     // console.log(username);
     // console.log(token);
 
-  var options = {
-    method: 'POST',
-    uri: 'https://trackobot.com/profile/results.json?username=' + this.username + '&token=' + this.token,
-    body: game,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    resolveWithFullResponse: true,
-    json: true
-  }
+    var options = {
+        method: 'POST',
+        uri: 'https://trackobot.com/profile/results.json?username=' + this.username + '&token=' + this.token,
+        body: game,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        resolveWithFullResponse: true,
+        json: true
+    }
 
-  request(options)
-        .then(function (response) {
-          let rsp = {
-            'success': true,
-            'code': response.statusCode,
-            'message': 'Game uploaded with success',
-            'data': response.body
-          }
-          responseReadyCallback(rsp)
-        }).catch(function (response) {
-          let rsp = {
-            'success': false,
-            'code': response.error.status,
-            'message': response.error.error,
-            'data': null
-          }
-          responseReadyCallback(rsp)
+    request(options)
+        .then(function(response) {
+            let rsp = {
+                'success': true,
+                'code': response.statusCode,
+                'message': 'Game uploaded with success',
+                'data': response.body
+            }
+            responseReadyCallback(rsp)
+        }).catch(function(response) {
+            let rsp = {
+                'success': false,
+                'code': response.error.status,
+                'message': response.error.error,
+                'data': null
+            }
+            responseReadyCallback(rsp)
         })
 }
 
-EasyTrackOBot.prototype.createNewUser = function (responseReadyCallback) {
-  var options = {
-    method: 'POST',
-    uri: 'https://trackobot.com/users.json',
-    resolveWithFullResponse: true
-  }
+EasyTrackOBot.prototype.createNewUser = function(responseReadyCallback) {
+    var options = {
+        method: 'POST',
+        uri: 'https://trackobot.com/users.json',
+        resolveWithFullResponse: true
+    }
 
-  request(options)
-        .then(function (response) {
-          let rsp = {
-            'success': true,
-            'code': response.statusCode,
-            'message': 'User created with success',
-            'data': response.body
-          }
-          responseReadyCallback(rsp)
-        }).catch(function (err) {
-          let rsp = {
+    request(options)
+        .then(function(response) {
+            let rsp = {
+                'success': true,
+                'code': response.statusCode,
+                'message': 'User created with success',
+                'data': response.body
+            }
+            responseReadyCallback(rsp)
+        }).catch(function(err) {
+            let rsp = {
+                'success': false,
+                'code': err.response.statusCode,
+                'message': err.response.statusMessage,
+                'data': null
+            }
+            responseReadyCallback(rsp)
+        })
+}
+
+EasyTrackOBot.prototype.getStats = function(
+    statsType,
+    timeRange,
+    mode,
+    startTime,
+    endTime,
+    asHero,
+    vsHero,
+    asDeck,
+    vsDeck,
+    responseReadyCallback) {
+
+    var allowedEndpoints = ['classes', 'decks', 'arena']
+    var allowedRange = ['current_month', 'all', 'last_3_days', 'last_24_hours', 'custom']
+    var allowedModes = ['ranked', 'arena', 'casual', 'friendly', 'all']
+
+
+    if (!allowedEndpoints.includes(statsType)) {
+        responseReadyCallback(rsp = {
             'success': false,
-            'code': err.response.statusCode,
-            'message': err.response.statusMessage,
-            'data': null
-          }
-          responseReadyCallback(rsp)
+            'message': "statsType must be one of " + allowedEndpoints
+        })
+        return false
+    }
+
+    if (!allowedRange.includes(timeRange)) {
+        responseReadyCallback(rsp = {
+            'success': false,
+            'message': "timeRange must be one of " + allowedRange
+        })
+        return false
+    }
+
+    if (!allowedModes.includes(mode)) {
+        responseReadyCallback(rsp = {
+            'success': false,
+            'message': "mode must be one of " + allowedModes
+        })
+        return false
+    }
+
+    var dateValidation = /(\d{4})-(\d{2})-(\d{2})/
+
+    if (timeRange == 'custom' && (!startTime.match(dateValidation) || (!endTime.match(dateValidation)))) {
+        responseReadyCallback(rsp = {
+            'success': false,
+            'message': "startTime and endTime must be of type 'yyyy-mm-dd' when using custom timeRange"
+        })
+    }
+
+    var bodyParams = {
+        'username': this.username,
+        'token': this.token,
+        'mode': mode,
+        'time_range': timeRange
+    }
+
+    if (timeRange == 'custom') {
+        bodyParams.start = startTime
+        bodyParams.end = endTime
+    }
+
+    if (statsType == 'decks') {
+        if (asDeck) bodyParams.as_deck = asDeck
+        if (vsDeck) bodyParams.vs_deck = vsDeck
+    } else if (statsType == 'classes') {
+        if (asHero) bodyParams.as_hero = asHero
+        if (vsHero) bodyParams.vs_hero = vsHero
+    }
+
+    //From object to url parameters ...&key=value&...
+    let objAsUrlParams = Object.keys(bodyParams).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(bodyParams[k])
+    }).join('&')
+
+    var options = {
+        method: 'GET',
+        uri: 'https://trackobot.com/profile/stats/' + statsType + '.json?' + objAsUrlParams,
+        body: bodyParams,
+        json: true
+    }
+
+    request(options)
+        .then(function(positiveResponse) {
+            let finalResponse = {
+                "success": true,
+                "data": positiveResponse
+            }
+            responseReadyCallback(finalResponse)
+        }).catch(function(negativeResponse) {
+            let finalResponse = {
+                "success": false,
+                "data": negativeResponse
+            }
+            responseReadyCallback(finalResponse)
         })
 }
 
--- test/index.js
+++ test/index.js
@@ -3,96 +3,121 @@ var EasyTrackOBot = require('../index.js')
 
 var easyTrackOBotObj
 
-test('initialize_object test', function (t) {
-  easyTrackOBotObj = new EasyTrackOBot('twilight-ysera-2686', 'Rg22pzUVixQpm1aXJZCV')
-  t.type(easyTrackOBotObj, 'object')
-  t.type(easyTrackOBotObj, 'EasyTrackOBot')
-  t.type(easyTrackOBotObj, EasyTrackOBot)
+test('initialize_object test', function(t) {
+    easyTrackOBotObj = new EasyTrackOBot('twilight-ysera-2686', 'Rg22pzUVixQpm1aXJZCV')
+    t.type(easyTrackOBotObj, 'object')
+    t.type(easyTrackOBotObj, 'EasyTrackOBot')
+    t.type(easyTrackOBotObj, EasyTrackOBot)
 
-  t.end()
+    t.end()
 })
 
-test('upload_game_OK test', function (t) {
-  const gameOfTestCorrect = {
-    'result': {
-      'mode': 'ranked',
-      'hero': 'Shaman',
-      'hero_deck': 'Token',
-      'opponent': 'Hunter',
-      'opponent_deck': 'Midrange',
-      'coin': true,
-      'win': true,
-      'duration': 228,
-      'card_history': []
+test('upload_game_OK test', function(t) {
+    const gameOfTestCorrect = {
+        'result': {
+            'mode': 'ranked',
+            'hero': 'Shaman',
+            'hero_deck': 'Token',
+            'opponent': 'Hunter',
+            'opponent_deck': 'Midrange',
+            'coin': true,
+            'win': true,
+            'duration': 228,
+            'card_history': []
+        }
     }
-  }
 
-  easyTrackOBotObj.uploadGame(
+    easyTrackOBotObj.uploadGame(
         gameOfTestCorrect,
         (result) => {
-          t.is(typeof result, 'object')
-          t.ok(Object.keys(result).length > 0)
+            t.is(typeof result, 'object')
+            t.ok(Object.keys(result).length > 0)
 
-          t.ok(result.hasOwnProperty('success'))
-          t.ok(result.hasOwnProperty('message'))
-          t.ok(result.hasOwnProperty('code'))
-          t.ok(result.hasOwnProperty('data'))
+            t.ok(result.hasOwnProperty('success'))
+            t.ok(result.hasOwnProperty('message'))
+            t.ok(result.hasOwnProperty('code'))
+            t.ok(result.hasOwnProperty('data'))
 
-          t.equal(result.success, true)
-          t.equal(result.message, 'Game uploaded with success')
-          t.equal(result.code, 201)
-          t.ok(result.data.hasOwnProperty('result'))
+            t.equal(result.success, true)
+            t.equal(result.message, 'Game uploaded with success')
+            t.equal(result.code, 201)
+            t.ok(result.data.hasOwnProperty('result'))
 
-          t.end()
+            t.end()
         }
     )
 })
 
-test('upload_game_KO test', function (t) {
-  const gameOfTestIncorrect = {
-    'result': ['This is obiviously not correct']
-  }
+test('upload_game_KO test', function(t) {
+    const gameOfTestIncorrect = {
+        'result': ['This is obiviously not correct']
+    }
 
-  easyTrackOBotObj.uploadGame(
+    easyTrackOBotObj.uploadGame(
         gameOfTestIncorrect,
         (result) => {
-          t.is(typeof result, 'object')
-          t.ok(Object.keys(result).length > 0)
+            t.is(typeof result, 'object')
+            t.ok(Object.keys(result).length > 0)
 
-          t.ok(result.hasOwnProperty('success'))
-          t.ok(result.hasOwnProperty('message'))
-          t.ok(result.hasOwnProperty('code'))
-          t.ok(result.hasOwnProperty('data'))
+            t.ok(result.hasOwnProperty('success'))
+            t.ok(result.hasOwnProperty('message'))
+            t.ok(result.hasOwnProperty('code'))
+            t.ok(result.hasOwnProperty('data'))
 
-          t.equal(result.success, false)
-          t.ok(result.code >= 300)
+            t.equal(result.success, false)
+            t.ok(result.code >= 300)
 
-          t.end()
+            t.end()
         }
     )
 })
 
-test('create_new_user test', function (t) {
-  easyTrackOBotObj.createNewUser(
+test('create_new_user test', function(t) {
+    easyTrackOBotObj.createNewUser(
         (result) => {
-          t.is(typeof result, 'object')
-          t.ok(Object.keys(result).length > 0)
+            t.is(typeof result, 'object')
+            t.ok(Object.keys(result).length > 0)
 
-          t.ok(result.hasOwnProperty('success'))
-          t.ok(result.hasOwnProperty('message'))
-          t.ok(result.hasOwnProperty('code'))
-          t.ok(result.hasOwnProperty('data'))
+            t.ok(result.hasOwnProperty('success'))
+            t.ok(result.hasOwnProperty('message'))
+            t.ok(result.hasOwnProperty('code'))
+            t.ok(result.hasOwnProperty('data'))
 
-          if (result.code === 200) {
-            t.equal(result.success, true)
-            t.ok(JSON.parse(result.data).hasOwnProperty('username'))
-            t.ok(JSON.parse(result.data).hasOwnProperty('password'))
-          } else if (result.code >= 300) {
-            t.equal(result.success, false)
-            t.equal(result.data, null)
-          }
+            if (result.code === 200) {
+                t.equal(result.success, true)
+                t.ok(JSON.parse(result.data).hasOwnProperty('username'))
+                t.ok(JSON.parse(result.data).hasOwnProperty('password'))
+            } else if (result.code >= 300) {
+                t.equal(result.success, false)
+                t.equal(result.data, null)
+            }
 
-          t.end()
+            t.end()
         }
     )
+})
+
+test('get_hearthstone_stats_full_OK test', function(t) {
+    easyTrackOBotObj.getStats(
+        "classes",
+        "all",
+        "all",
+        undefined,
+        undefined,
+        "Hunter",
+        "Druid",
+        undefined,
+        undefined,
+        (result) => {
+            t.is(typeof result, 'object')
+            t.ok(Object.keys(result).length > 0)
+
+            t.ok(result.hasOwnProperty('success'))
+            t.ok(result.hasOwnProperty('data'))
+            t.ok(result.data.hasOwnProperty('stats'))
+
+            t.equal(result.success, true)
+
+            t.end()
+        })
 })
